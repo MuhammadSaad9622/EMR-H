39609999889aafeef6acf70f541c1c8c6dd2784b
MuhammadSaad <saadnadeem962@gmail.com>
2025-08-06 19:04:47 +0500

    .

--- server/index.js
+++ server/index.js
@@ -36,7 +36,8 @@ const PORT = process.env.PORT || 5000;
 // CORS configuration
 const allowedOrigins = [
   'http://localhost:5173', // Local development
-  'https://haraold.vercel.app', // Your Vercel app
+  'https://haraold.vercel.app',
+  'https://emr-h-lime.vercel.app', // Your Vercel app
   // Add other domains as needed
 ];
 
